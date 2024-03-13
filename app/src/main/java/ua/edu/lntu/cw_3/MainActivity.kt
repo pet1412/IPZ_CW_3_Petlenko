0de8235fa4117b0f7527fefa951dae315357aafc
pet1412 <162765300+pet1412@users.noreply.github.com>
2024-03-13 14:47:33 +0200

    Добавив відображення за допомогою LazyColumn

--- app/src/main/java/ua/edu/lntu/cw_3/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw_3/MainActivity.kt
@@ -6,7 +6,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -15,6 +19,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ua.edu.lntu.cw_3.ui.theme.IPZ_CW_3_PetlenkoTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,7 +41,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(modifier: Modifier = Modifier) {
-    Item()
+    LazyColumn(
+        contentPadding = PaddingValues(15.dp),
+        content = {
+            items(3){
+                Item()
+            }
+        }
+    )
 }
 
 @Preview(showBackground = true)
@@ -57,6 +69,7 @@ fun Item(){
             contentDescription = null
         )
         Text(text = stringResource(id = R.string.day1_bottom))
+        Spacer(modifier = Modifier.height(12.dp))
 
     }
 }
