b4d3685850164a56a70757df75303e55469ced11
pet1412 <162765300+pet1412@users.noreply.github.com>
2024-03-13 15:31:44 +0200

    Попробував зробити анімацію, уви не вийшло, не хоче запускатись сама програма

--- app/src/main/java/ua/edu/lntu/cw_3/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw_3/MainActivity.kt
@@ -4,21 +4,44 @@ import android.content.ClipData.Item
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.SizeTransform
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.core.keyframes
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.with
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import ua.edu.lntu.cw_3.ui.theme.IPZ_CW_3_PetlenkoTheme
 
@@ -45,7 +68,9 @@ fun Greeting(modifier: Modifier = Modifier) {
         contentPadding = PaddingValues(15.dp),
         content = {
             items(3){
-                Item()
+                index ->
+                Item(index = index)
+                Spacer(modifier = Modifier.height(12.dp))
             }
         }
     )
@@ -59,17 +84,57 @@ fun GreetingPreview() {
     }
 }
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
-fun Item(){
-    Column {
-        Text(text = stringResource(id = R.string.day1_day))
-        Text(text = stringResource(id = R.string.day1_name))
-        Image(
-            painter = painterResource(id = R.drawable.day_1),
-            contentDescription = null
-        )
-        Text(text = stringResource(id = R.string.day1_bottom))
-        Spacer(modifier = Modifier.height(12.dp))
+fun Item(index: Int){
+    var expanded by remember { mutableStateOf(false) }
+    Box(
+        modifier = Modifier
+            .clip(RoundedCornerShape(50.dp))
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(15.dp)
+                .background(Color.Gray)
+        ){
 
+            Text(text = stringResource(id = R.string.day1_day))
+            Text(text = stringResource(id = R.string.day1_name))
+            Image(
+                painter = painterResource(id = R.drawable.day_1),
+                contentDescription = null,
+                modifier = Modifier
+                    .clip(RoundedCornerShape(14.dp))
+            )
+            AnimatedContent(
+                targetState = expanded,
+                transitionSpec = {
+                    fadeIn(animationSpec = tween(150, 150)) with
+                            fadeOut(animationSpec = tween(150)) using
+                            SizeTransform { initialSize, targetSize ->
+                                if (targetState) {
+                                    keyframes {
+                                        // Expand horizontally first.
+                                        IntSize(targetSize.width, initialSize.height) at 150
+                                        durationMillis = 300
+                                    }
+                                } else {
+                                    keyframes {
+                                        // Shrink vertically first.
+                                        IntSize(initialSize.width, targetSize.height) at 150
+                                        durationMillis = 300
+                                    }
+                                }
+                            }
+                }
+            ) { targetExpanded ->
+                if (targetExpanded) {
+                    Text(text = stringResource(id = R.string.day1_bottom))
+                } else {
+                    println("Hello World")
+                }
+            }
+        }
     }
 }
+
